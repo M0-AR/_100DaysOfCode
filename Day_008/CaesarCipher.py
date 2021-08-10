ba65b292606b3930209d5ee4ea85e80bd6b738fa
m0-ar <s176492@student.dtu.dk>
2021-08-10 20:14:56 +0200

    Prompt the user to put an input.

--- Day_008/CaesarCipher.py
+++ Day_008/CaesarCipher.py
@@ -1,18 +1,18 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-# text = input("Type your message:\n").lower()
-# shift = int(input("Type the shift number:\n"))
 
 # Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(text, shift_amount):
+def encrypt(start_text, shift_amount):
     # Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and
     # print the encrypted text. e.g. plain_text = "hello" shift = 5 cipher_text = "mjqqt" print output: "The encoded
     # text is mjqqt"
     cipher_text = ""
-    for char in text:
+    for char in start_text:
         cipher_text += alphabet[(alphabet.index(char) + shift_amount) % 26]
     return cipher_text
 
@@ -23,16 +23,17 @@ def encrypt(text, shift_amount):
 
 
 # Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
-print("Encrypt -> abc by 50 ", encrypt("abc", 50))
+if direction == "encode":
+    print("Encrypt -> abc by: 50 ", encrypt(text, shift))
 
 
 # Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
-def decrypt(text, shift_amount):
+def decrypt(start_text, shift_amount):
     # Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount
     # and print the decrypted text. e.g. cipher_text = "mjqqt" shift = 5 plain_text = "hello" print output: "The
     # decoded text is hello"
     plain_text = ""
-    for char in text:
+    for char in start_text:
         plain_text += alphabet[(26 - abs((alphabet.index(char) - shift_amount))) % 26]
     return plain_text
 
@@ -40,4 +41,5 @@ def decrypt(text, shift_amount):
 # Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the
 # correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a
 # message.
-print("decrypt -> yza by 50 ", decrypt("yza", 50))
+if direction == "decode":
+    print("decrypt -> yza by 50: ", decrypt(text, shift))
