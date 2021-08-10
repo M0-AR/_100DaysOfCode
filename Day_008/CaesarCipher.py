621fad4b6ebd830b7c26685fd1740db6613be413
m0-ar <s176492@student.dtu.dk>
2021-08-10 20:10:30 +0200

    Refactor parameter name.

--- Day_008/CaesarCipher.py
+++ Day_008/CaesarCipher.py
@@ -7,13 +7,13 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 # shift = int(input("Type the shift number:\n"))
 
 # Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(text, shift):
+def encrypt(text, shift_amount):
     # Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and
     # print the encrypted text. e.g. plain_text = "hello" shift = 5 cipher_text = "mjqqt" print output: "The encoded
     # text is mjqqt"
     cipher_text = ""
     for char in text:
-        cipher_text += alphabet[(alphabet.index(char) + shift) % 26]
+        cipher_text += alphabet[(alphabet.index(char) + shift_amount) % 26]
     return cipher_text
 
     ##HINT: How do you get the index of an item in a list:
@@ -27,13 +27,13 @@ print("Encrypt -> abc by 50 ", encrypt("abc", 50))
 
 
 # Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
-def decrypt(text, shift):
+def decrypt(text, shift_amount):
     # Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount
     # and print the decrypted text. e.g. cipher_text = "mjqqt" shift = 5 plain_text = "hello" print output: "The
     # decoded text is hello"
     plain_text = ""
     for char in text:
-        plain_text += alphabet[(26 - abs((alphabet.index(char) - shift))) % 26]
+        plain_text += alphabet[(26 - abs((alphabet.index(char) - shift_amount))) % 26]
     return plain_text
 
 
