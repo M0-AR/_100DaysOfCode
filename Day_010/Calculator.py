e25bb34fa109fa53f98677e7f4034236458eb706
m0-ar <s176492@student.dtu.dk>
2021-08-18 02:36:06 +0200

    Refactor name and bug fixing.

--- Day_010/Calculator.py
+++ Day_010/Calculator.py
@@ -1,49 +1,53 @@
 from replit import clear
-import art
+from art import logo
 
 
 def add(n1, n2):
     return n1 + n2
 
 
-def sub(n1, n2):
+def subtract(n1, n2):
     return n1 - n2
 
 
-def mul(n1, n2):
+def multiply(n1, n2):
     return n1 * n2
 
 
-def div(n1, n2):
+def divide(n1, n2):
     return n1 / n2
 
 
-continueWithOldResult = False
-result = 0
-while True:
-    if not continueWithOldResult:
-        clear()
-        print(art.logo)
-        firstNumber = int(input("What is the first number?: "))
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide,
+}
 
-        print("+\n-\n*\n/")
-    else:
-        firstNumber = result
 
-    operation = input("Pick an operation: ")
+def calculator():
+    print(logo)
 
-    secondNumber = int(input("What is the next number?: "))
+    num1 = float(input("What is the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
-    if operation == '+':
-        result = add(firstNumber, secondNumber)
-    elif operation == '-':
-        result = sub(firstNumber, secondNumber)
-    elif operation == '*':
-        result = mul(firstNumber, secondNumber)
-    elif operation == '/':
-        result = div(firstNumber, secondNumber)
+    while should_continue:
+        operation = input("Pick an operation: ")
+        num2 = float(input("What is the next number?: "))
 
-    print(f"{firstNumber} {operation} {secondNumber} = {result}")
+        function = operations[operation]
+        result = function(num1, num2)
 
-    choose = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
-    continueWithOldResult = choose == 'y'
+        print(f"{num1} {operation} {num2} = {result}")
+
+        if input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = result
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
