69c2889d23b91a581b2146d5ba7e821da1fb873d
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-12-19 16:34:22 +0100

    requirements 4, 5, 6, 7, and 8 are done

--- Day_011/BlackjackGame.py
+++ Day_011/BlackjackGame.py
@@ -2,80 +2,149 @@ import random
 from art import logo
 
 
-def random_number():
+def deal_card():
     """Return a random card from the deck of cards."""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    random_num = random.choice(cards)
-    return cards[random_num]
+    card = random.choice(cards)
+    return card
 
 
-def sum_value_of_cards(cards):
-    sum_cards = 0
-    for value in cards:
-        sum_cards += value
-    return sum_cards
+# Hint 5: Deal the user and computer 2 cards each using deal_card()
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
 
 
+# 6
 def calculate_score(cards):
-    # Hint 7
-    if sum_value_of_cards(cards) == 21 and len(cards) == 2:
+    # 7
+    if check_balckjack(cards):
         return 0
-    # Hint 8
-    if sum_value_of_cards(cards) > 21 and 11 in cards:
-        cards.remove(11)
-        cards.append(1)
-
-    return sum_value_of_cards(cards)
+    # 8
+    if check_for_ace(cards) and sum(cards) > 21:
+        replace_ace_by(cards, 1)
+    return sum(cards)
 
 
-def blackjack_game():
-    print(logo)
-    user_cards = []
-    computer_cards = []
-
-    user_cards.append(random_number())
-    user_cards.append(random_number())
-
-    computer_cards.append(random_number())
-
-    # Print user and computer cards array
-    print(f"Your cards: {user_cards}")
-    print(f"Computer's first card: {computer_cards}")
-
-    # Prompt user
-    get_another_card = input("Type 'y' to get another card, type 'n' to pass: ")
-
-    # Check input
-    if get_another_card == 'y':
-        user_cards.append(random_number())
-
-    computer_cards.append(random_number())
-
-    # Print user and computer cards array
-    print(f"Your final hand: {user_cards}")
-    print(f"Computer's final hand: {computer_cards}")
-
-    # Calculate result
-    sum_user_cards = sum_value_of_cards(user_cards)
-    sum_computer_cards = sum_value_of_cards(computer_cards)
-
-    user_diff_from_21 = 21 - sum_user_cards
-    computer_diff_from_21 = 21 - sum_computer_cards
-
-    # Display result
-    if sum_user_cards > 21 or sum_computer_cards == 21:
-        print("You Lose")
-    elif sum_computer_cards > 21 or sum_user_cards == 21:
-        print("You Win")
-    elif computer_diff_from_21 < user_diff_from_21:
-        print("You Lose")
-    elif user_diff_from_21 < computer_diff_from_21:
-        print("You Win")
-
-    # Ask for playing again
-    play_again = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
-    if play_again == 'y':
-        blackjack_game()
+# 7
+def check_balckjack(cards):
+    return check_for_ace(cards) and 10 in cards and len(cards)
 
 
-blackjack_game()
+# 8
+def check_for_ace(cards):
+    return 11 in cards
+
+def replace_ace_by(cards, replacement):
+    cards.remove(11)
+    cards.append(replacement)
+#
+#
+# def sum_value_of_cards(cards):
+#     sum_cards = 0
+#     for value in cards:
+#         sum_cards += value
+#     return sum_cards
+#
+#
+# def calculate_score(cards):
+#     # Hint 7
+#     if sum_value_of_cards(cards) == 21 and len(cards) == 2:
+#         return 0
+#     # Hint 8
+#     if sum_value_of_cards(cards) > 21 and 11 in cards:
+#         cards.remove(11)
+#         cards.append(1)
+#
+#     return sum_value_of_cards(cards)
+#
+#
+# def blackjack_game():
+#     print(logo)
+#     user_cards = []
+#     computer_cards = []
+#
+#     user_cards.append(random_number())
+#     user_cards.append(random_number())
+#
+#     computer_cards.append(random_number())
+#
+#     # Print user and computer cards array
+#     print(f"Your cards: {user_cards}")
+#     print(f"Computer's first card: {computer_cards}")
+#
+#     # Prompt user
+#     get_another_card = input("Type 'y' to get another card, type 'n' to pass: ")
+#
+#     # Check input
+#     if get_another_card == 'y':
+#         user_cards.append(random_number())
+#
+#     computer_cards.append(random_number())
+#
+#     # Print user and computer cards array
+#     print(f"Your final hand: {user_cards}")
+#     print(f"Computer's final hand: {computer_cards}")
+#
+#     # Calculate result
+#     sum_user_cards = sum_value_of_cards(user_cards)
+#     sum_computer_cards = sum_value_of_cards(computer_cards)
+#
+#     user_diff_from_21 = 21 - sum_user_cards
+#     computer_diff_from_21 = 21 - sum_computer_cards
+#
+#     # Display result
+#     if sum_user_cards > 21 or sum_computer_cards == 21:
+#         print("You Lose")
+#     elif sum_computer_cards > 21 or sum_user_cards == 21:
+#         print("You Win")
+#     elif computer_diff_from_21 < user_diff_from_21:
+#         print("You Lose")
+#     elif user_diff_from_21 < computer_diff_from_21:
+#         print("You Win")
+#
+#     # Ask for playing again
+#     play_again = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+#     if play_again == 'y':
+#         blackjack_game()
+#
+#
+# # blackjack_game()
+# import math
+#
+# arr = []
+# arr1 = []
+# num = int(input("Enter number of inputs:"))
+# # num=20
+# # print("a".split(" "))
+# for i in range(num):
+#     inp = input()
+#     arr.append(inp)
+#
+# # arr=["R 10","R 3","L 1", "R 4","L 9", "E" ,"E", "R 3", "R 3", "R 3", "E" ,"E" ,"E" ,"E" ,"E" ,"E"]
+# # arr=["R 441","R 1477","E", "R 415","E", "R 5357" ,"E", "E", "R 7816", "E", "L 7724","E", "R 444","E", "R 4203","E", "L 132","E", "L 7181","E"]
+# # arr=["L 6681","L 1255","R 2625", "L 1736","L 2573", "E" ,"E", "L 8141", "E", "R 7051"]
+# for i in range(num):
+#     a = arr[i].split(" ")
+#     if len(a) > 1:
+#         letter = a[0]
+#         digit = int(a[1])
+#     else:
+#         letter = a[0]
+#         digit = float('inf')
+#
+#     if digit != float('inf'):
+#         if letter == "R":
+#             arr1.append(digit)
+#             # print(arr1)
+#         elif letter == "L":
+#             arr1.insert(0, digit)
+#             # print(arr1)
+#     elif digit == float('inf'):
+#         if letter == "E":
+#             ind = math.ceil(len(arr1) / 2) - 1
+#             print(arr1.pop(ind))
+#             # print(arr1)
--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
+# Global variables, I intentionally leave the values as ... because # I don't have any meaningful values yet. But it won't affect how
+# you understand the problem, I promise.
+R, C = ...
+m = ...
+sr, sc = ...
+rq, cq = ...
+# Variables used to keep track of total number of steps to be taken
+move_count = 0
+nodes_left_in_layer = 0
+nodes_in_next_layer = 1
+# Variable to see whether we already reached at the end or not
+reached_end = False
+# Matrix to keep track of visited cells.
+visited = ...
+# North, South, East and West direction vectors
+dr = [-1, +1, 0, 0]
+dc = [0, 0, +1, -1]
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def solve():
+    rq.enqueue(sr)
+    cq.enqueue(sc)
+    visited[sr][sc] = True
 
+    while rq.size() > 0:
+        r = rq.dequeue()
+        c = cq.dequeue()
+        if m[r][c] == 'E':
+            reached_end = True
+            break
+        explore_neighbors(r, c)
+        nodes_left_in_layer -= 1
+        if nodes_left_in_layer == 0:
+            nodes_left_in_layer = nodes_in_next_layer
+            nodes_in_next_layer = 0
+            move_count += 1
+    if reached_end == True:
+        return move_count
+    return -1
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def explore_neighbors(r, c):
+    for i in 4:
+        rr = r + dr[i]
+        cc = c + dc[i]
 
+        if rr < 0 or cc < 0:
+            continue
+        if rr >= R or cc >= C:
+            continue
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        if visited[r][c] == True:
+            continue
+        if m[r][c] == '#':
+            continue
 
-#  See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        rq.enqueue(rr)
+        cq.enqueue(cc)
+        visited[r][c] =True
+
+        nodes_in_next_layer += 1
