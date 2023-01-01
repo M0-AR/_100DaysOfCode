e5c967ca3b06039f9532a5ad80496e51cdaf6a40
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2023-01-01 10:23:13 +0100

    Clean the code

--- Day_014/higher_lower.py
+++ Day_014/higher_lower.py
@@ -9,25 +9,41 @@ isAnswerRight = True
 # Print logo
 print(logo)
 
+
+def format_data(account):
+    """Takes the account data and returns the printable format."""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
+
+
+def check_who_has_more_followers(a_followers, b_followers):
+    if a_followers > b_followers:
+        return 'a'
+    else:
+        return 'b'
+
+
 while isAnswerRight:
-    # Generate a random number
-    a = random.randint(0, len(data) - 1)
-    b = random.randint(0, len(data) - 1)
+    # Pick a random object
+    a = random.choice(data)
+    b = random.choice(data)
+
+    # If two objects are same try again to pick different objects
     if a == b:
         continue
 
     # Ask the user
-    print(f'Compare A: {data[a]["name"]}, {data[a]["description"]}, {data[a]["country"]}')
+    print(f'Compare A: {format_data(a)}')
     print(vs)
-    print(f'Compare B: {data[b]["name"]}, {data[b]["description"]}, {data[b]["country"]}')
+    print(f'Compare B: {format_data(b)}')
 
-    answer = input("Who has more followers? Type 'A' or 'B':")
+    user_answer = input("Who has more followers? Type 'A' or 'B':").lower()
+    correct_answer = check_who_has_more_followers(a["follower_count"], b["follower_count"])
 
     # Check the answer
-    if answer == 'A':
-        isAnswerRight = data[a]["follower_count"] > data[b]["follower_count"]
-    else:
-        isAnswerRight = data[b]["follower_count"] > data[a]["follower_count"]
+    isAnswerRight = user_answer == correct_answer
 
     # Display result
     if isAnswerRight:
